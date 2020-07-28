9aa9a908cc5a3df999f82aa2d8c41daf2197eb24
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-07-28 13:55:13 -0300

    slider

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-function Menu(){
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img src={Logo} className="Logo" alt="VitinFlix logo" />
-            </Link>
-            
-            <Button as={Link} to="/cadastro/video" className="ButtonLink">
-                Novo vídeo
-            </Button>
-        </nav>        
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
